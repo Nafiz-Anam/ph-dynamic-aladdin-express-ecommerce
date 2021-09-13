17da84becd511f19746659c15c34ca15ab147344
Nafiz Anam <nafiz.anam002@gmail.com>
2021-09-13 17:20:17 +0600

    All bug fixed

--- js/app.js
+++ js/app.js
@@ -13,16 +13,22 @@ const showProducts = (products) => {
         const image = product.image;
         const div = document.createElement("div");
         div.classList.add("product");
-        div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+        div.innerHTML = `
+        <div class="single-product">
+            <div>
+                <img class="product-image" src=${image}></img>
+            </div>
+            <div class="product-body">
+                <h1>${product.title}</h1>
+                <p>Category: ${product.category}</p>
+                <h3>Price: $ ${product.price}</h3>
+                <p>Total Reviews: <b>${product.rating.count}</b></p>
+                <p>Rating: <b>${product.rating.rate}</b></p>
+                <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+                <button id="details-btn" class="btn btn-danger">Details</button>
+            </div>
+        </div>
+        `;
         document.getElementById("all-products").appendChild(div);
     }
 };
@@ -31,14 +37,14 @@ let count = 0;
 const addToCart = (id, price) => {
     count = count + 1;
     updatePrice("price", price);
-
     updateTaxAndCharge();
     document.getElementById("total-Products").innerText = count;
+    updateTotal();
 };
 
 const getInputValue = (id) => {
     const element = document.getElementById(id).innerText;
-    const converted = parseInt(element);
+    const converted = parseFloat(element);
     return converted;
 };
 
@@ -47,12 +53,12 @@ const updatePrice = (id, value) => {
     const convertedOldPrice = getInputValue(id);
     const convertPrice = parseFloat(value);
     const total = convertedOldPrice + convertPrice;
-    document.getElementById(id).innerText = Math.round(total);
+    document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-    document.getElementById(id).innerText = Math.round(value);
+    document.getElementById(id).innerText = parseFloat(value).toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -78,5 +84,6 @@ const updateTotal = () => {
         getInputValue("price") +
         getInputValue("delivery-charge") +
         getInputValue("total-tax");
-    document.getElementById("total").innerText = grandTotal;
+    document.getElementById("total").innerText =
+        parseFloat(grandTotal).toFixed(2);
 };
